2de714bc60aa941d4ba4d1b7d509dd5f32a0db3b
Vinícius Pereira <viniciusofp@gmail.com>
2025-09-29 22:11:06 -0300

    GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-KVMC4R69" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-muted font-sans`}
       >
